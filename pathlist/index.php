08ed8624d958f007b9cb909b0a9cde9c0b31c62d
NORRY94 <orywu1994@gmail.com>
2025-06-30 05:01:29 +0700

    hasil setelah menyelesaikan masalah berjam-jam

--- pathlist/index.php
+++ pathlist/index.php
@@ -5,13 +5,25 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
 
 // Koneksi ke database
-$conn = mysqli_connect("localhost", "root", "", "api-belajar");
+$conn = mysqli_connect("localhost", "root", "", "tk_azzhara_pku");
 
-$QSquery = mysqli_query($conn, "SELECT * FROM pegawai");
+$QSquery = mysqli_query($conn, "SELECT * FROM test");
 $allData = [];
 
 while ($row = mysqli_fetch_assoc($QSquery)) {
     $allData[] = $row;
 }
+
+$input = file_get_contents("php://input");
+$data = json_decode($input, true);
+$id = isset($data["id"]) ? $data["id"] : null;
+$nama = isset($data["nama"]) ? $data["nama"] : null;
+$kelas = isset($data["kelas"]) ? $data["kelas"] : null;
+
+if (isset($nama, $kelas)) {
+
+    mysqli_query($conn, "UPDATE test SET kelas='$kelas', nama='$nama' WHERE id='$id'");
+}
+
 
 echo json_encode($allData);
--- pathlist/script.js
+++ pathlist/script.js
@@ -1,22 +1,76 @@
-const no = document.getElementById("no");
-const id = document.getElementById("id");
-const nama = document.getElementById("nama");
-const kelas = document.getElementById("kelas");
 const tbody = document.getElementById("tbody");
+
+const editModal = document.getElementById("editModal");
+const closeBtn = document.querySelector(".close");
+const edit = document.getElementById("edit");
+const editNama = document.getElementById("edit-nama");
+const editKelas = document.getElementById("edit-kelas");
+const editForm = document.getElementById("editForm");
+const editId = document.getElementById("edit-id");
 
 const viewData = async () => {
   const res = await fetch("http://localhost/projectTkAzzhara/pathlist/");
   const result = await res.json();
   console.log(result);
+
   result.forEach((data, index) => {
     const tr = document.createElement("tr");
     tr.classList.add("data-row");
-    tr.innerHTML = `<td  class="data-cell">${index + 1}</td>
-                    <td  class="data-cell">${data.id}</td>
-                    <td  class="data-cell">${data.nama}</td>
-                    <td  class="data-cell">${data.email}</td>
+
+    tr.innerHTML = `
+      <td class="data-cell">${index + 1}</td>
+      <td class="data-cell" id="id">${data.id}</td>
+      <td class="data-cell nama">${data.nama}</td>
+      <td class="data-cell kelas">${data.kelas}</td>
+      <td class="data-cell">
+        <button onclick='editData(${JSON.stringify(data.id)})'>Edit</button>
+        <button >Delete</button>
+      </td>
+      <td><button>Download</button></td>
     `;
+
     tbody.appendChild(tr);
   });
 };
 viewData();
+
+// edit data
+const editData = (dataId) => {
+  editModal.style.display = "block";
+  editId.value = dataId;
+};
+closeBtn.onclick = () => {
+  editModal.style.display = "none";
+};
+
+window.onclick = (e) => {
+  if (e.target == editModal) {
+    editModal.style.display = "none";
+  }
+};
+
+// tekan btn submit modal form
+editForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const data = {
+    id: editId.value,
+    nama: editNama.value,
+    kelas: editKelas.value,
+  };
+  try {
+    const res = await fetch("http://localhost/projectTkAzzhara/pathlist/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await res.json();
+    tbody.innerHTML = "";
+    editModal.style.display = "none";
+    viewData();
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+});
